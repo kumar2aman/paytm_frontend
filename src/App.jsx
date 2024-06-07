2084500fa0ba8f83014da660759266de804b843b
kumar2aman <singh2aman07@gmail.com>
2024-06-08 02:06:29 +0530

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, {Component, Fragment} from 'react';
 import './App.css'
-import { Router, Route, RouterProvider, BrowserRouter, Routes } from 'react-router-dom'
+import { Router, Route, RouterProvider, BrowserRouter, Routes,  } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin';
 import Homepage from './pages/Homepage';
 import Dashboard from './pages/Dashboard';
 import SendMoney from './pages/SendMoney';
 import Transfer from './pages/Transfer';
+
 
 
 function App() {
@@ -16,14 +17,14 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
-    <switch>
+   
     <Route path='/' element={<Homepage/>}/>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/signin' element={<Signin/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
       <Route path="/send" element={<SendMoney />} />
       <Route path="/transfer" element={<Transfer/>} />
-    </switch>
+   
       
       
     </Routes>
